9b033d5dcbb720731e4eece41d26857af4979c59
unknown <metybuza46@gmail.com>
2020-11-07 16:23:05 -0700

    updated file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div className="m-10 text-sm text-right text-gray-500">
-           <p>Designed by  <a href="https://github.com/metasabeya" className="font-bold text-red-600 hover:text-white-700">Mety Ketsela</a></p>
+           <p>Designed by Mety Ketsela</p>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,9 @@ function Header() {
         <div className="bg-white shadow text-gray-800">
             <header className="container max-w-6xl px-10 py-3 mx-auto flex justify-between items-center">
                 <div className="flex items-center">
-                    {/* <img className="mr-3" src={logo} alt="UCF School Logo"/> */}
+                   
                     <h1 className="font-bold">Employee Directory</h1>
+                   
                 </div>
                
             </header>
