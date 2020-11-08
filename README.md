87044237b76ab6a1d0cb5fabaf31f0cbd5f6370e
unknown <metybuza46@gmail.com>
2020-11-07 21:32:25 -0700

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Employee Directory
+This is an employee directory app that enable you to to see the entire employee at once so you will have quick access to thier 
+
+information.
+
+Technology used for this app is nodejs, react, axios.
+
+It is a table of random user, when the user load the page , the table of employee will render.
+
+
+The user is able to sort th table by category or using their name.
+ 
--- src/App.js
+++ src/App.js
@@ -1,63 +1,34 @@
-import React, { useState } from 'react';
-import Header from "./components/Header";
-import Navigation from "./components/Navigation";
-import Layout from "./components/Layout";
-import EmployeeCardList from "./components/EmployeeCardList";
-import Footer from "./components/Footer";
-import employees from "./data/employees.json";
+import React from 'react';
+import './App.css';
+import axios from "axios";
+import SiteHead from './components/SiteHead';
+import Navbar from './components/Navbar';
 
-function App() {
-    const [ searchTerm, setSearchTerm ] = useState("");
-    const [ sorted, setSorted] = useState(false);
-    const [ data, setEmployees ] = useState(employees);
 
-    function handleSearchTerm(event)  {
-        setSearchTerm(event.target.value)
-    }
+class App extends React.Component {
 
-    function handleSortByName() {
-        // sort array ascending or descending by first name
-        if (!sorted) {
-            setEmployees(data.sort((a, b) => (a.name > b.name) ? 1 : -1));
-            setSorted(true);
-        } else {
-            setEmployees(data.sort((a, b) => (a.name > b.name) ? -1 : 1));
-            setSorted(false);
-        }
-    }
+  state = {
+    employees: [],
+  };
 
-    function handleSortByDept() {
-        // sort array ascending or descending by dept name
-        if (!sorted) {
-            setEmployees(data.sort((a, b) => (a.department > b.department) ? 1 : -1));
-            setSorted(true);
-        } else {
-            setEmployees(data.sort((a, b) => (a.department > b.department) ? -1 : 1));
-            setSorted(false);
-        }
-    }
+  componentDidMount() {
+      axios.get(`https://randomuser.me/api/?results=20&nat=Aus`)
+        .then(res => {
+          this.setState({ employees: res.data.results });
+        });
+  }
 
-    // the filteredEmployees variable only stores employee names that start with with the matching string you type
-    const filteredEmployees = data.filter(employee => employee.name.toLowerCase().startsWith(searchTerm.toLowerCase()));
+
+  render() {
     return (
-        <div>
-            <Header/>
-            <Layout>
-                <h1 className="title text-5xl text-gray-800 mt-16">Employee Directory</h1>
-                <p className="mb-16 text-md">Search for an employee or sort by Name or Category.</p>
-                {/*the handleSearchTerm method and searchTerm state get passed down to the Navigation component via props with the onSearch and searchTerm props*/}
-                <Navigation
-                    onSearch={handleSearchTerm}
-                    searchTerm={searchTerm}
-                    handleSortByName={handleSortByName}
-                    handleSortByDept={handleSortByDept}
-                />
-                {/* the employees array gets the filteredEmployees data via the data prop */}
-                <EmployeeCardList data={filteredEmployees}/>
-                <Footer/>
-            </Layout>
-        </div>
-    )
+      <div className="App">
+        <SiteHead />
+        {this.state.employees.length > 0 &&
+        <Navbar employees={this.state.employees}/>
+  }
+      </div>
+    );
+  }
 }
 
 export default App;
